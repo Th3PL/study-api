f17d7ca42df48c7b2fb476781e776cd4ab313107
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-17 22:44:09 -0300

    Aula 1 -correcao

---
+++ src/main/java/com/github/th3pl/study_apix/StudyApixApplication.java
@@ -0,0 +1,15 @@
+package com.github.th3pl.study_apix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApixApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/github/th3pl/study_apix/StudyApixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.th3pl.study_apix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
